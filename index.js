0ee9d000599216b35b6f0a63729a96d9c84900bf
Arnav Saha <44087308+sahaarnav3@users.noreply.github.com>
2024-04-23 19:06:18 +0530

    Added both the endpoints and their functionality..

--- index.js
+++ index.js
@@ -24,6 +24,24 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+//creating below endpoint for fetching only with date
+app.get("/api/:dateString", (req, res) => {
+  // console.log(req.params.year + " " +req.params.month + " " +req.params.date);
+  const dateString = req.params.dateString;
+  //below if else to check if unix is sent or a date
+  let date = "";
+  if (dateString.split("-").length > 1) {
+    date = new Date(dateString);
+  } else {
+    date = new Date(parseInt(dateString));
+  }
+  const unixCode = date.getTime(); // the result is in milliseconds (divide by 1000 to get in seconds)
+  const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+  const dayName = days[date.getDay()];
+  const monthName = months[date.getUTCMonth()];
+  res.json({ unix: unixCode, utc: `${dayName}, ${date.getDate()} ${monthName} ${date.getFullYear()} 00:00:00 GMT` });
+})
 
 
 // Listen on port set in environment variable or default to 3000
